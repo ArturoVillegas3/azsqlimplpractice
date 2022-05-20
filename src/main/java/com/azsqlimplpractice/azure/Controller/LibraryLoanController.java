4492bf0acdf24cc6c01fcc65ff38ede54cebc0ce
ArturoVillegas3 <percentnevada3@gmail.com>
2022-05-20 00:20:26 -0500

    Third

--- src/main/java/com/azsqlimplpractice/azure/Controller/LibraryLoanController.java
+++ src/main/java/com/azsqlimplpractice/azure/Controller/LibraryLoanController.java
@@ -6,10 +6,7 @@ import com.azsqlimplpractice.azure.Service.LibraryLoanService;
 import com.azsqlimplpractice.azure.Service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LibraryLoanController {
